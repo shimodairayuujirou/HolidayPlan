7ffe17053e654cadcf8fbeb80f0743cd4bce727a
下平裕次郎 <shimodairayuu@gmail.com>
2024-10-17 17:02:03 +0900

    Initial Commit

---
+++ HolidayPlan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HolidayPlan
+//
+//  Created by 下平裕次郎 on R 6/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HolidayPlanTests/HolidayPlanTests.swift
@@ -0,0 +1,17 @@
+//
+//  HolidayPlanTests.swift
+//  HolidayPlanTests
+//
+//  Created by 下平裕次郎 on R 6/10/17.
+//
+
+import Testing
+@testable import HolidayPlan
+
+struct HolidayPlanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
