e0e911fbaf34b9752b54f6094d577c25d0b0c487
下平裕次郎 <shimodairayuu@gmail.com>
2024-12-12 16:35:17 +0900

    HomePageの大まかな形はできた

--- HolidayPlan/View/HomeView.swift
+++ HolidayPlan/View/HomeView.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    private let Label: UILabel = {
+        let label = UILabel()
+        label.text = "ホーム"
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 30)
+        label.textColor = .white
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.mainColor
